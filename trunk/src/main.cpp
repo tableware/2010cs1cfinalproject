d9ea5268b4350cf0817bc5b2cb55cdaec0627ffb
noblestgibbin <noblestgibbin@3a35303a-4bf9-9824-4d6c-0ca86e2594b0>
2010-04-16 03:58:06 +0000

    Project skeleton =)

---
+++ trunk/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 12, 2010
+ *      Author: jgibbins1
+ */
+
+#include "header.h"
+
+int Winery::numOfWineries = 0;
+
+int main()
+{
+	cout << Winery::numOfWineries << endl;
+
+
+	return 0;
+}
