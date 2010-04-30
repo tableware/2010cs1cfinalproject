8009d8c22c2d62c36794cc6f39bf3259d6b1cbc5
darkstar1103 <darkstar1103@3a35303a-4bf9-9824-4d6c-0ca86e2594b0>
2010-04-30 03:15:13 +0000

    Trip stub

---
+++ trunk/src/classes/trip.h
@@ -0,0 +1,38 @@
+/*
+ * trip.h
+ *
+ *  Created on: Apr 29, 2010
+ *      Author: darkstar1103
+ */
+
+#ifndef TRIP_H_
+#define TRIP_H_
+
+class trip
+{
+	struct winePurchase
+	{
+		Wine* wine;
+		int quantity;
+	};
+
+	public:
+		trip();
+		~trip();
+
+		float getSubTotal();
+		float getTax();
+		float getTotal();
+
+		void process();
+
+		vector<Winery*> wineries;
+		vector<winePurchase> boozeList;
+
+	private:
+		float subTotal;
+		float tax;
+		float distanceTraveled;
+};
+
+#endif /* TRIP_H_ */
