2e15bec47ba24198203e3b271713b90b3ad6ce55
darkstar1103 <darkstar1103@3a35303a-4bf9-9824-4d6c-0ca86e2594b0>
2010-05-03 21:17:18 +0000

    Fixed trip so that it would compile

--- src/classes/libraries/trip.h
+++ src/classes/libraries/trip.h
@@ -8,35 +8,38 @@
 #ifndef TRIP_H_
 #define TRIP_H_
 
-//#include "../../header.h"
+#include "../../header.h"
 
-//class trip
-//{
-//	struct winePurchase
-//	{
-//		Wine* wine;
-//		int quantity;
-//	};
-//
-//	public:
-//		trip();
-//		~trip();
-//
-//		float getSubTotal();
-//		float getTax();
-//		float getTotal();
-//
-//		void process(vector<Winery*> mainList);
-//
-//		int findShortestDist(vector<WineryDistance*> distanceList);
-//
-//		vector<Winery*> wineries;
-//		vector<winePurchase> boozeList;
-//
-//	private:
-//		float subTotal;
-//		float tax;
-//		float distanceTraveled;
-//};
+// do a forward declare
+class Winery;
+
+class trip
+{
+	struct winePurchase
+	{
+		Wine* wine;
+		int quantity;
+	};
+
+	public:
+		trip();
+		~trip();
+
+		float getSubTotal();
+		float getTax();
+		float getTotal();
+
+		void process(vector<Winery*> mainList);
+
+		int findShortestDist(vector<WineryDistance*> distanceList);
+
+		vector<Winery*> wineries;
+		vector<winePurchase> boozeList;
+
+	private:
+		float subTotal;
+		float tax;
+		float distanceTraveled;
+};
 
 #endif /* TRIP_H_ */
