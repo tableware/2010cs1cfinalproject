095fa89c8aed7ac3d49d4820922901cc92156363
noblestgibbin <noblestgibbin@3a35303a-4bf9-9824-4d6c-0ca86e2594b0>
2010-05-03 20:44:56 +0000

    Added the find function templates FindName and FindNum. I added a demo in the test code of how they work.

--- src/classes/libraries/trip.h
+++ src/classes/libraries/trip.h
@@ -8,31 +8,35 @@
 #ifndef TRIP_H_
 #define TRIP_H_
 
-class trip
-{
-	struct winePurchase
-	{
-		Wine* wine;
-		int quantity;
-	};
+//#include "../../header.h"
 
-	public:
-		trip();
-		~trip();
-
-		float getSubTotal();
-		float getTax();
-		float getTotal();
-
-		void process();
-
-		vector<Winery*> wineries;
-		vector<winePurchase> boozeList;
-
-	private:
-		float subTotal;
-		float tax;
-		float distanceTraveled;
-};
+//class trip
+//{
+//	struct winePurchase
+//	{
+//		Wine* wine;
+//		int quantity;
+//	};
+//
+//	public:
+//		trip();
+//		~trip();
+//
+//		float getSubTotal();
+//		float getTax();
+//		float getTotal();
+//
+//		void process(vector<Winery*> mainList);
+//
+//		int findShortestDist(vector<WineryDistance*> distanceList);
+//
+//		vector<Winery*> wineries;
+//		vector<winePurchase> boozeList;
+//
+//	private:
+//		float subTotal;
+//		float tax;
+//		float distanceTraveled;
+//};
 
 #endif /* TRIP_H_ */
